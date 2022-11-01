42af3701d55dac467854efebd0fcaefd9479a294
Arul Mathur <arulmathur1@gmail.com>
2022-10-31 21:02:17 -0700

    Test PR Again

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import Twemoji from './Twemoji'
 
 const user = {
-    name: 'Tim Cooky',
+    name: 'Tim Cookies',
     email: 'tom@example.com',
     imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
